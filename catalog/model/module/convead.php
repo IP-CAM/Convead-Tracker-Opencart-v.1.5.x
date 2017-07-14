6ed84af3461a910348e25ad6ecd77b95632cdfcc
Saveljev Vladimir <v.saveljev@secoint.ru>
2017-07-14 14:37:39 +0300

    Fix for variant id

--- catalog/model/module/convead.php
+++ catalog/model/module/convead.php
@@ -21,8 +21,8 @@ class ModelModuleConvead extends Model {
   public function getVariantId($product_id, $options) {
     if (count($options)) {
       $option = current($options);
-      if (!isset($option['option_value_id']) and isset($option['option_value']) and is_array($option['option_value'])) $option = current($option['option_value']);
-      return $product_id.(isset($option['option_value_id']) ? str_pad($option['option_value_id'], 7, '0', STR_PAD_LEFT) : '');
+      if (!isset($option['product_option_value_id']) and isset($option['option_value']) and is_array($option['option_value'])) $option = current($option['option_value']);
+      return $product_id.(isset($option['product_option_value_id']) ? str_pad($option['product_option_value_id'], 6, '0', STR_PAD_LEFT) : '');
     }
     else {
       return $product_id;
